54a3fca683129a53acae144844a01c455ecd04c2
Parul Jaiswal <paruljansie@gmail.com>
2024-03-06 11:20:19 +0530

    handled cors issue

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ const productionRouter = require('./routes/production/production')
 
 //express app
 const app = express()
-app.use(cors({
-    origin: '*', // Allow requests from any origin (you can replace '*' with your frontend URL)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specified methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-    credentials: true // Enable sending cookies across origins if needed
-}));
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 //middleware
 app.use(express.json())
 app.use((req, res, next) => {
